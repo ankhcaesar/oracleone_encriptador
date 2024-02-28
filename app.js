dd2d8818c0a904b2625884ad3a6fd3e125fa8c07
ankhcaesar <ankhcaesar@gmail.com>
2024-02-28 18:10:00 -0300

    creando js

--- app.js
+++ app.js
@@ -6,7 +6,27 @@ const matriz = [
     ["u", "ufat"],
 ];
 
-let campoTexto = document.querySelector("#ta-texto-encriptar");
-let campoMensaje = document.querySelector("#ta-texto-desencriptado");
+const campoTexto = document.querySelector("#ta-texto-encriptar");
+const campoMensaje = document.querySelector("#ta-texto-desencriptado");
 console.log(campoMensaje, campoTexto)
 
+
+
+/**botones */
+function botonencriptado(){
+
+}
+
+function botondesencriptar(){
+
+}
+
+function botoncopiar(){
+navigator.clipboard.writeText(campoMensaje);
+}
+
+/** funciones varias */
+function mostrartextodesencriptado(){
+    document.getElementsByClassName("desencriptado")[0].style.display = "inline-block";
+    document.getElementsByClassName("sndesencriptado")[0].style.display = "none";
+}
